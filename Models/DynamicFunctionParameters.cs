252ce7a0032dae6fd5dea771b71eea82677a89fa
Aidan Bleser <aidanbleser35@gmail.com>
2023-09-13 11:12:48 -0500

    Added Required Methods

---
+++ Models/DynamicFunctionParameters.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Postgres.Models
+{
+    /// <summary>
+    /// Extends Dapper's DynamicParameters but modifies the behavior slightly so that every attribute is added to the list.
+    /// <code>
+    /// // Use [NotMapped] to prevent mapping
+    /// [NotMapped]
+    /// public int Age { get; set; } 
+    /// </code>
+    /// </summary>
+    public class DynamicFunctionParameters : DynamicParameters
+    {
+        public DynamicFunctionParameters() : base() { }
+
+        public DynamicFunctionParameters(object obj)
+        {
+            Type type = obj.GetType();
+
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.GetCustomAttribute(typeof(NotMappedAttribute)) == null)
+                {
+                    this.Add(property.Name, property.GetValue(obj));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a string with the parameterized function parameters.
+        /// 
+        /// <code>
+        /// string function = "add_user";
+        /// 
+        /// string query = $"SELECT * FROM {function}({parameters.GetParameterList()});";
+        /// 
+        /// Console.WriteLine(query); // SELECT * FROM add_user(username := @username, email := @email);
+        /// </code>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetParameterList()
+        {
+            string param = "";
+
+            if (this != null)
+            {
+                var paramNames = this.ParameterNames.ToList();
+
+                for (int i = 0; i < paramNames.Count; i++)
+                {
+                    if (i > 0)
+                    {
+                        param += $", {paramNames[i]} := @{paramNames[i]}";
+                    }
+                    else
+                    {
+                        param += $"{paramNames[i]} := @{paramNames[i]}";
+                    }
+                }
+            }
+
+            return param;
+        }
+    }
+}
---
+++ NpgsqlConnectionExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Dapper;
+using Dapper.Postgres.Models;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Postgres
+{
+    public static class NpgsqlConnectionExtensions
+    {
+        /// <summary>
+        /// Calls a Postgres function as a parameterized query and returns a list of the specified type. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="connection"></param>
+        /// <param name="function"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static async Task<List<T>> QueryFunctionAsync<T>(this NpgsqlConnection connection, string function, DynamicFunctionParameters? parameters = null)
+        {
+            string param = "";
+            if (parameters != null)
+            {
+                param = parameters.GetParameterList();
+            }
+
+            string query = $"SELECT * FROM {function}({param});";
+
+            var result = await connection.QueryAsync<T>(query, parameters);
+
+            return result.ToList();
+        }
+
+        /// <summary>
+        /// Calls a Postgres function as a parameterized query and returns an item of the specified type;
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="connection"></param>
+        /// <param name="function"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static async Task<T> QueryFunctionFirstAsync<T>(this NpgsqlConnection connection, string function, DynamicFunctionParameters? parameters = null)
+        {
+            string param = "";
+            if (parameters != null)
+            {
+                param = parameters.GetParameterList();
+            }
+
+            string query = $"SELECT * FROM {function}({param});";
+
+            T result = await connection.QueryFirstAsync<T>(query, parameters);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Calls a Postgres function as a parameterized query and returns a item of the specified type. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="connection"></param>
+        /// <param name="function"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static async Task<T> ExecuteFunctionAsync<T>(this NpgsqlConnection connection, string function, DynamicFunctionParameters? parameters = null)
+        {
+            string param = "";
+            if (parameters != null)
+            {
+                param = parameters.GetParameterList();
+            }
+
+            string query = $"SELECT {function}({param});";
+
+            var result = await connection.QuerySingleOrDefaultAsync<T>(query, parameters);
+
+            return result;
+        }
+    }
+}
